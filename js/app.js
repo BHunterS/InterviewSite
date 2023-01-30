4863f325ca49645c0e8aff65e1ed3fd7f33a79de
BHunterS <69817804+BHunterS@users.noreply.github.com>
2023-01-30 10:26:47 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,31 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    const mainSection = document.querySelector(".main");
+    document.addEventListener("scroll", (() => {
+        const scrollTop = window.scrollY;
+        if (0 !== scrollTop) mainSection.classList.add("_scrolled"); else mainSection.classList.remove("_scrolled");
+    }));
+    isWebp();
+})();
